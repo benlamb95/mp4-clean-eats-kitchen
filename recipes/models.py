90d17dac03436607da11da359f9636cb3d84d86a
benlamb95 <benl1995@hotmail.co.uk>
2022-06-13 16:33:02 +0000

    Create recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Added"))
+
+
+class Recipe(models.Model):
+    owner = models.ForeignKey(User,  
+                              on_delete=models.CASCADE,
+                              null=False,
+                              blank=False,
+                              related_name='owner')
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    description = models.CharField(max_length=300, blank=True, null=True)
+    recipe_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User,
+        related_name="recipe_likes",
+        blank=True
+    )
