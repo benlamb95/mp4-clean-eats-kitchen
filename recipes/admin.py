4a16c865484bbc9b4a410b83d5cd7be1ac3a9aed
benlamb95 <benl1995@hotmail.co.uk>
2022-06-14 12:56:18 +0000

    Adjust recipe and ingredient model  so calories and macros entered only once. Admin site complete

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient, Step, Comment
 
 # Register your models here.
+# https://www.youtube.com/watch?v=hpo18rVZhG4&list=PLEsfXFp6DpzRMby_cSoWTFw8zaMdTEXgL&index=51
+
+
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+
+
+class RecipeStep(admin.StackedInline):
+    model = Step
+    extra = 0
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    fieldsets = []
+    inlines = [RecipeIngredientInline, RecipeStep]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'recipe', 'added_on', 'approved')
+    list_filter = ('approved', 'added_on')
+    search_fields = ('name', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
+admin.site.register(RecipeIngredient)
+admin.site.register(Step)
