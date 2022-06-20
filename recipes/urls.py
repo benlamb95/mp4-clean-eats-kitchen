74543c42ba004407e8b1f37a8a2b892398868bc7
benlamb95 <benl1995@hotmail.co.uk>
2022-06-20 11:40:53 +0000

    Created all recipes tempate and connect URL. Also link the nav bar with url

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
         views.RecipeList.as_view(),
         name='home'
     ),
+    path('recipes/all_recipes', views.AllRecipes.as_view(), name='all_recipes'),
     path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_view'),
 ]
