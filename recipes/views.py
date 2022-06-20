5020f2865302171251388731578f1e6c426a40f6
benlamb95 <benl1995@hotmail.co.uk>
2022-06-20 08:59:43 +0000

    Views.py: Create view so users can view individual recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -7,3 +7,25 @@ class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')[0:8]
     template_name = 'index.html'
+
+
+class RecipeDetails(View):
+    """View to see indivdual recipe"""
+    def get(self, request, sug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approve=True).order_by('created_on')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_view.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+            },
+        )
