cb6a199feb6df2ac226e937cefaa1f10fea2bcf6
benlamb95 <benl1995@hotmail.co.uk>
2022-06-20 16:56:37 +0000

    Add like and unlike button functionality

--- recipes/urls.py
+++ recipes/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     ),
     path('recipes/all_recipes', views.AllRecipes.as_view(), name='all_recipes'),
     path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_view'),
+    path('like/<slug:slug>/', views.RecipeLike.as_view(), name='recipe_likes'),
 ]
