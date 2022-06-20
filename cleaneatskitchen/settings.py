d38ea8cbbe21023f998cbf34838a2db22415b8ef
benlamb95 <benl1995@hotmail.co.uk>
2022-06-20 15:00:08 +0000

    Add comment functionality with help from codeinstitute walk through project

--- cleaneatskitchen/settings.py
+++ cleaneatskitchen/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'recipes',
 ]
 
@@ -55,6 +56,8 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
